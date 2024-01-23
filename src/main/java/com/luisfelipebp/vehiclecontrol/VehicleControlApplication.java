139ef47cdd186bad9b24dc60451f16bb0f67902b
luisfelipebp <luisfelipebarbosa2013@gmail.com>
2024-01-23 15:29:48 -0300

    first commit

---
+++ src/main/java/com/luisfelipebp/vehiclecontrol/VehicleControlApplication.java
@@ -0,0 +1,13 @@
+package com.luisfelipebp.vehiclecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisfelipebp/vehiclecontrol/VehicleControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisfelipebp.vehiclecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
